30590f8b4eecfff06cf0cb50fe8c6229433a5b9d
YonugJones <peterckerfoot@gmail.com>
2025-01-18 10:38:36 -0600

    set up index entry file for backend

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+// imports and requires
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+// Application level middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+// Routes
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to the backend API' })
+});
+
+const port = process.env.PORT || 3333;
+app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
