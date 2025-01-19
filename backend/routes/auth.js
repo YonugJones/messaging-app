4413cb6692b70516c282f02faf7fde07c2c55c87
YonugJones <peterckerfoot@gmail.com>
2025-01-19 12:09:24 -0600

    added login to auth route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { signup } = require('../controllers/authController');
-const { validateSignup } = require('../middleware/validateInput');
+const { signup, login } = require('../controllers/authController');
+const { validateSignup, validateLogin } = require('../middleware/validateInput');
 const router = express.Router();
 
 router.post('/signup', validateSignup, signup);
+router.post('/login', validateLogin, login);
 
 module.exports = router;
