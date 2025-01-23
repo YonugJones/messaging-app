6f3f2f3522c4702f691a7f539e9ddaa8feb9201b
YonugJones <peterckerfoot@gmail.com>
2025-01-22 21:05:54 -0600

    added message icon to header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,7 +1,11 @@
+import { faMessage } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 const Header = () => {
   return (
     <header className='header'>
       <h1>Messaging App</h1>
+      <FontAwesomeIcon icon={faMessage} />
     </header>
   )
 };
