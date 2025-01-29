4ef317420bac871c86801339ab974956da88a85a
YonugJones <peterckerfoot@gmail.com>
2025-01-29 12:47:08 -0600

    created useAxiosPrivate for attaching interceptors to protected api routes

--- frontend/src/hooks/useAxiosPrivate.js
+++ frontend/src/hooks/useAxiosPrivate.js
@@ -1,46 +1,46 @@
-// // hook used return axiosPrivate instance after attaching interceptors
+import { axiosPrivate } from '../api/axios';
+import useAuth from './useAuth';
+import useRefreshToken from '../hooks/useRefreshToken';
 
-// // import { axiosPrivate } from '../api/axios';
-// import { useEffect } from 'react';
-// import useRefreshToken from './useRefreshToken';
-// import useAuth from './useAuth';
+const useAxiosPrivate = () => {
+  const { auth } = useAuth();
+  const refresh = useRefreshToken();
 
-// const useAxiosPrivate = () => {
-//   const refresh = useRefreshToken();
-//   const { auth } = useAuth();
+  // attach accessToken to req header IF there is no req header AND if user's auth contains accessToken
+  axiosPrivate.interceptors.request.use(
+    async (config) => {
+      if (!config.headers['Authorization'] && auth?.accessToken) {
+        config.headers['Authorization'] = `Bearer ${auth.accessToken}`;
+      }
+      return config;
+    },
+    (error) => Promise.reject(error)
+  );
 
-//   useEffect(() => {
-//     const requestIntercept = axiosPrivate.interceptors.request.use(
-//       (config) => {
-//         // if authorization header doesn't exist, this is the first attempt, not a retry
-//         if (!config.headers['Authorization']) {
-//           config.headers['Authorization'] = `Bearer ${auth.accessToken}`;
-//         }
-//         return config
-//       }, (error) => Promise.reject(error)
-//     )
+  // get ready to attach to response
+  axiosPrivate.interceptors.response.use(
+    // if response return true, do nothing to it
+    (response) => response,
+    // if error
+    async (error) => {
+      // define prevRequest as error config
+      const prevRequest = error?.config;
+      // if error resopnse is 403 and response has been sent ONCE
+      if (error?.response?.status === 403 && !prevRequest?.sent) {
+        prevRequest.sent = true;
+        // generate newAccessToken
+        const newAccessToken = refresh();
+        // if success
+        if (newAccessToken) {
+          // set access token in response headers
+          prevRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
+          return axiosPrivate(prevRequest);
+        }
+      }
+      return Promise.reject(error);
+    }
+  );
+  return axiosPrivate;
+}
 
-//     const responseIntercept = axiosPrivate.interceptors.response.use(
-//       (response) => response,
-//       async (error) => {
-//         const prevRequest = error?.config;
-//         if (error?.response?.status === 403 && !prevRequest?.sent) {
-//           prevRequest.sent = true // we set to true so the request does not attempt more than twice
-//           const newAccessToken = await refresh();
-//           prevRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
-//           return axiosPrivate(prevRequest);
-//         }
-//         return Promise.reject(error);
-//       }
-//     )
-
-//     return () => {
-//       axiosPrivate.interceptors.request.eject(requestIntercept);
-//       axiosPrivate.interceptors.response.eject(responseIntercept);
-//     }
-//   }, [auth, refresh]);
-
-//   return axiosPrivate
-// };
-
-// export default useAxiosPrivate;
+export default useAxiosPrivate;
